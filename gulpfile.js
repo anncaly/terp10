c1f6f9e1cfbc1e404fdaebbbe7c5c9e08121fb20
anncaly <anncaly@gmail.com>
2017-04-05 14:59:52 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const gulp           = require('gulp');
+const pug            = require('gulp-pug');
+const mainBowerFiles = require('main-bower-files');
+const clean          = require('gulp-clean');
+const html2pug       = require('gulp-html2pug');
+
+var paths = {
+    scss:['./src/scss/main.scss'],
+    pug:['./src/index.pug'],
+}
+
+gulp.task('clean', function(done) {
+    gulp.src('./build/*', { read: false })
+        .pipe(clean())
+    done(); 
+});
+
+gulp.task('fonts', function(done) {
+    gulp.src(mainBowerFiles(["**/*.{otf,eot,ttf,woff,woff2,svg}"],{
+        "overrides": {
+            "bootstrap-sass": {
+                "main": [
+                    "./assets/fonts/bootstrap/*.*"
+                ]
+             },
+            "font-awesome": {
+                "main": [
+                    "fonts/*.*"
+                ]
+             }
+        }            
+    }))
+        .pipe(gulp.dest('./build/fonts'))
+    done(); 
+});
+
+gulp.task('js', function(done) {
+    gulp.src(mainBowerFiles(["**/*.js"],{
+        "overrides": {
+            "bootstrap-sass": {
+                "main": [
+                    "./assets/javascripts/bootstrap.min.js"
+                ]
+             },
+            "jquery": {
+                "main": [
+                    "dist/jquery.min.js"
+                ]
+             }
+        }            
+    }))
+        .pipe(gulp.dest('./build/js'))
+    done(); 
+});
+
+gulp.task('pug', function(done) {
+    gulp.src('src/index.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./build'))
+    done();
+});
+
+gulp.task('html2pug', function(done) {
+    return gulp.src('./src/temp/*.html')
+        .pipe(html2pug())
+        .pipe(gulp.dest('./src'))
+    done();
+});
+
+gulp.task('pug:watch', function() {
+    gulp.watch(paths.pug, gulp.series('pug'));
+//    gulp.watch(paths.scss, gulp.series('scss'));
+});
+
+gulp.task('build', gulp.series('clean', 'pug', 'fonts', 'js', function(done) {
+    done();
+}));
